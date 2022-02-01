a336c33a14557897eabcb92a02a3a88f0726d1f8
Shubham Adep <shubham.adep@yahoo.com>
2022-01-31 23:23:24 -0800

    Time: 34 ms (51.61%), Space: 13.9 MB (98.89%) - LeetHub

---
+++ 670-maximum-swap/670-maximum-swap.py
@@ -0,0 +1,28 @@
+#https://leetcode.com/problems/maximum-swap/discuss/846837/Python-3-or-Greedy-Math-or-Explanations
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        s = list(str(num))
+        
+        i = 0
+        while i < len(s)-1:
+            if s[i] < s[i+1]:
+                break
+            i += 1
+            
+        if i == len(s) - 1:
+            return num
+        
+        max_idx, max_val = i+1, s[i+1]
+        for j in range(i+1, len(s)):
+            if s[j] >= max_val:
+                max_val = s[j]
+                max_idx = j
+        
+        min_idx = i
+        for j in range(i, -1, -1):
+            if s[j] < max_val:
+                min_idx = j
+        
+        s[min_idx], s[max_idx] = s[max_idx], s[min_idx]
+        return int(''.join(s))
+                
