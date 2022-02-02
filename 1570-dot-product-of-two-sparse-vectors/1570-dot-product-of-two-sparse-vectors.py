6610329906b0652ff70c8603b26dd20f4025d9cb
Shubham Adep <shubham.adep@yahoo.com>
2022-02-02 01:59:15 -0800

    Time: 2725 ms (30.08%), Space: 18.6 MB (49.94%) - LeetHub

---
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.py
@@ -0,0 +1,15 @@
+class SparseVector:
+    def __init__(self, nums: List[int]):
+        self.nonzeros = {}
+        for i, n in enumerate(nums):
+            if n != 0:
+                self.nonzeros[i] = n              
+
+    def dotProduct(self, vec: 'SparseVector') -> int:
+        result = 0
+        # iterate through each non-zero element in this sparse vector
+        # update the dot product if the corresponding index has a non-zero value in the other vector
+        for i, n in self.nonzeros.items():
+            if i in vec.nonzeros:
+                result += n * vec.nonzeros[i]
+        return result
