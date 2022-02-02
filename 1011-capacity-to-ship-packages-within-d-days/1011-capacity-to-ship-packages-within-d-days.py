b1a56cad580691a20393b3405d0c702bcf17f782
Shubham Adep <shubham.adep@yahoo.com>
2022-02-02 00:43:17 -0800

    Time: 506 ms (86.18%), Space: 17.2 MB (7.97%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,28 @@
+# idea: https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/discuss/1581292/Well-Explained-oror-Thought-process-oror-94-faster
+
+# great template for binary search questions: https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/discuss/769698/Python-Clear-explanation-Powerful-Ultimate-Binary-Search-Template.-Solved-many-problems.
+
+class Solution:
+    def shipWithinDays(self, weights: List[int], D: int) -> int:
+        
+        def feasible(capacity):
+            days = 1
+            tempCapacity = 0
+            
+            for w in weights:
+                tempCapacity += w
+                if tempCapacity > capacity:
+                    tempCapacity = w
+                    days += 1
+                    if days > D:
+                        return False
+            return True
+        
+        left, right = max(weights), sum(weights)
+        while left < right:
+            mid = left + (right-left) // 2
+            if feasible(mid):
+                right = mid
+            else:
+                left = mid+1
+        return left
