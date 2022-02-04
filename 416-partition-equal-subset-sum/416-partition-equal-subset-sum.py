ffa5652e82ea9980875f0b7fa60a5df03b270910
Shubham Adep <shubham.adep@yahoo.com>
2022-02-03 21:05:12 -0800

    Time: 1059 ms (54.26%), Space: 68.1 MB (15.45%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -0,0 +1,43 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        @lru_cache(maxsize=None)
+        def dfs(nums: Tuple[int], n: int, subset_sum: int) -> bool:
+            # Base cases
+            if subset_sum == 0:
+                return True
+            if n == 0 or subset_sum < 0:
+                return False
+            result = (dfs(nums, n - 1, subset_sum - nums[n - 1])
+                    or dfs(nums, n - 1, subset_sum))
+            return result
+
+        # find sum of array elements
+        total_sum = sum(nums)
+
+        # if total_sum is odd, it cannot be partitioned into equal sum subsets
+        if total_sum % 2 != 0:
+            return False
+
+        subset_sum = total_sum // 2
+        n = len(nums)
+        return dfs(tuple(nums), n - 1, subset_sum)
+    
+    
+# class Solution:
+#     def canPartition(self, nums: List[int]) -> bool:
+#         # find sum of array elements
+#         total_sum = sum(nums)
+
+#         # if total_sum is odd, it cannot be partitioned into equal sum subsets
+#         if total_sum % 2 != 0:
+#             return False
+#         subset_sum = total_sum // 2
+
+#         # construct a dp table of size (subset_sum + 1)
+#         dp = [False] * (subset_sum + 1)
+#         dp[0] = True
+#         for curr in nums:
+#             for j in range(subset_sum, curr - 1, -1):
+#                 dp[j] = dp[j] or dp[j - curr]
+
+#         return dp[subset_sum]
