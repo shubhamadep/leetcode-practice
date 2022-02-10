4bf937a36ca538d85800e158236c16bd22c25fe7
Shubham Adep <shubham.adep@yahoo.com>
2022-02-09 23:18:35 -0800

    Time: 49 ms (37.72%), Space: 13.9 MB (93.24%) - LeetHub

---
+++ 984-string-without-aaa-or-bbb/984-string-without-aaa-or-bbb.py
@@ -0,0 +1,24 @@
+class Solution:
+    def strWithout3a3b(self, a: int, b: int) -> str:
+        d={'a':a,'b':b}
+        h=[]
+        for k,v in d.items():
+            if v>0:
+                heapq.heappush(h,(-v,k))
+        res=''
+        while h:
+            top=heapq.heappop(h)
+            if len(res)>1 and top[1]==res[-1] and top[1]==res[-2]:
+                if not h:
+                    break
+                second=heapq.heappop(h)
+                heapq.heappush(h,(-d[top[1]],top[1]))
+                top=second
+            res+=top[1]
+            d[top[1]]-=1
+            if d[top[1]]>0:
+                heapq.heappush(h,(-d[top[1]],top[1]))
+                
+        return res
+        
+        
