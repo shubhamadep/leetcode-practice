c95f22bf6812169f24abed964dea4ded805bfc17
Shubham Adep <shubham.adep@yahoo.com>
2022-02-09 23:02:09 -0800

    Time: 1251 ms (12.47%), Space: 18.2 MB (19.69%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
+        reserved = dict()
+        for row, col in reservedSeats:
+            if row in reserved:
+                reserved[row].add(col)
+            else:
+                reserved[row] = {col}
+        groups = 0
+        for r, set_reserved in reserved.items():
+            if not set_reserved & {2, 3, 4, 5}:
+                groups += 1
+                if not set_reserved & {6, 7, 8, 9}:
+                    groups += 1
+            elif not set_reserved & {6, 7, 8, 9}:
+                groups += 1
+            elif not set_reserved & {4, 5, 6, 7}:
+                groups += 1
+        #empty rows which do not exist in the reserved dict()
+        return groups + (n - len(reserved)) * 2
