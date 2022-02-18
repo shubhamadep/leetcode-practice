9469904ac4fbf91ab1bf821009f05de1c2e2a1d6
Shubham Adep <shubham.adep@yahoo.com>
2022-02-17 18:22:15 -0800

    Time: 34 ms (66.06%), Space: 14 MB (68.38%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.py
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.py
@@ -1,14 +1,14 @@
-class Solution(object):
-    def minAddToMakeValid(self, s):
-        ans = bal = 0
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        stack = []
         for char in s:
-            if char == '(':
-                bal += 1
-            else:
-                bal -= 1
-            
-            if bal == -1:
-                ans += 1
-                bal += 1
-        return ans + bal
-        
+            if not stack:
+                stack.append(char)
+            elif char == '(':
+                stack.append(char)
+            elif char == ')':
+                if stack[-1] == '(':
+                    stack.pop()
+                else:
+                    stack.append(char)
+        return len(stack)
