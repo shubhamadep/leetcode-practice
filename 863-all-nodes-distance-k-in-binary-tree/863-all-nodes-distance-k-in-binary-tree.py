dc9d82e4ce2a9a6e2c184aa7d3993144e9c333f2
Shubham Adep <shubham.adep@yahoo.com>
2022-02-19 18:10:38 -0800

    Time: 44 ms (64.56%), Space: 14.2 MB (99.40%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.py
@@ -0,0 +1,35 @@
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, K: int) -> List[int]:
+        
+        ans = []
+
+        def dfs(node):
+            if not node:
+                return -1
+            elif node is target:
+                subtree_add(node, 0)
+                return 1
+            else:
+                L, R = dfs(node.left), dfs(node.right)
+                if L != -1:
+                    if L == K: ans.append(node.val)
+                    subtree_add(node.right, L + 1)
+                    return L + 1
+                elif R != -1:
+                    if R == K: ans.append(node.val)
+                    subtree_add(node.left, R + 1)
+                    return R + 1
+                else:
+                    return -1
+
+        def subtree_add(node, dist):
+            if not node:
+                return
+            elif dist == K:
+                ans.append(node.val)
+            else:
+                subtree_add(node.left, dist + 1)
+                subtree_add(node.right, dist + 1)
+
+        dfs(root)
+        return ans
