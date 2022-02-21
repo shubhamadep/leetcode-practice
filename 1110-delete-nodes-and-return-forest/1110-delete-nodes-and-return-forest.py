5493bdece9ebb939ca1fde9f17b16d2b9f4948f7
Shubham Adep <shubham.adep@yahoo.com>
2022-02-20 20:21:02 -0800

    Time: 111 ms (30.39%), Space: 14.5 MB (75.58%) - LeetHub

---
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.py
@@ -0,0 +1,29 @@
+class Solution:
+    def delNodes(self, root: TreeNode, to_delete: List[int]) -> List[TreeNode]:     
+        self.to_delete = set(to_delete)
+        self.result = []
+        
+        root = self.dfs(root)
+        if root.val not in self.to_delete:
+            self.result.append(root)
+        return self.result
+    
+    def dfs(self, root):
+        if not root:
+            return
+        
+        root.left = self.dfs(root.left)
+        root.right = self.dfs(root.right)
+        
+        if root.left and root.left.val in self.to_delete:
+            self.to_delete.remove(root.left.val)
+            root.left = None
+            
+        if root.right and root.right.val in self.to_delete:
+            self.to_delete.remove(root.right.val)
+            root.right = None
+        
+        if root.val in self.to_delete:
+            if root.left: self.result.append(root.left)
+            if root.right: self.result.append(root.right)
+        return root
