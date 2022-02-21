fdef0a5d6a7437ad68089ba1e93623544e2b7900
Shubham Adep <shubham.adep@yahoo.com>
2022-02-20 23:28:47 -0800

    Time: 77 ms (34.52%), Space: 14.4 MB (95.29%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        def helper(node):
+            if not node:
+                return None, 0
+            
+            left_node, left_depth = helper(node.left)
+            right_node, right_depth = helper(node.right)
+            
+            if left_depth > right_depth:
+                return left_node, left_depth + 1
+            elif right_depth > left_depth:
+                return right_node, right_depth + 1
+            
+            return node, left_depth+1
+        
+        node, depth = helper(root)
+        
+        return node
