74fd32fb1f2f551cb02dd6b84b8a71feacf18f1b
Shubham Adep <shubham.adep@yahoo.com>
2022-02-21 22:14:43 -0800

    Time: 2035 ms (74.31%), Space: 23.7 MB (66.78%) - LeetHub

---
+++ 2071-maximum-number-of-tasks-you-can-assign/2071-maximum-number-of-tasks-you-can-assign.py
@@ -0,0 +1,44 @@
+# solution copied from https://leetcode.com/problems/maximum-number-of-tasks-you-can-assign/discuss/1620435/Python-3-solution
+
+# very hard problem - only for good to know
+
+class Solution:
+    def maxTaskAssign(self, T: List[int], W: List[int], pills: int, strength: int) -> int:
+        
+        
+        def isPossible(T,W,pills,strength):
+
+            harderTask, strongestMan = len(T) - 1, -1
+            
+            for _ in range(len(T)):
+                if W[strongestMan] >= T[harderTask]:
+                    W.pop()
+                else:
+                    if not pills: return False
+                    power_needed = T[harderTask] - strength
+                    index = bisect.bisect_left(W,power_needed)
+                    if index >= len(W): return False
+                    pills -= 1
+                    W.pop(index)
+                harderTask -= 1
+                
+            return True
+        
+        
+                    
+        T.sort()
+        W.sort()
+        
+        l,h, = 0, min(len(T),len(W))
+        
+        while l <= h:
+            mid = (l+h) // 2
+
+            res = isPossible(T[:mid], W[len(W)-mid:], pills, strength) 
+            
+            if res: l = mid + 1
+            else: h = mid - 1
+                
+        return h
+            
+        
