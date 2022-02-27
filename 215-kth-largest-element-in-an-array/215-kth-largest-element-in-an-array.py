6e13c64348dc64d23bd74593d3cd398094a6e5f6
Shubham Adep <shubham.adep@yahoo.com>
2022-02-27 15:43:11 -0800

    Time: 2015 ms (9.94%), Space: 14.8 MB (88.25%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,26 +1,40 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        def helper(nums, start, end, k):
+        
+        def quick_select(start, end, k):
+
             while True:
-                pivot = start
+                
                 left = start + 1
                 right = end
-                while right >= left:
-                    if nums[left] > nums[pivot] and nums[right] < nums[pivot]:
+                pivot = start
+                
+                while left <= right:
+                    left_num, pivot_num, right_num = nums[left], nums[pivot], nums[right]
+                    
+                    if pivot_num < left_num and pivot_num > right_num:
                         nums[left], nums[right] = nums[right], nums[left]
-                    elif nums[left] <= nums[pivot]:
-                        left += 1
-                    elif nums[right] >= nums[pivot]:
+                    
+                    elif pivot_num <= right_num:
                         right -= 1
+                    
+                    elif pivot_num >= left_num:
+                        left += 1
+                
                 nums[pivot], nums[right] = nums[right], nums[pivot]
+                
                 if right == k:
                     return nums[right]
-                elif k > right:
-                    start = right+1
+                
+                if k > right:
+                    start = right + 1
+                
                 else:
-                    end = right-1
+                    end = right - 1
+            
             return -1
-                         
+        
         if k > len(nums):
             return -1
-        return helper(nums, 0, len(nums)-1, len(nums)-k)
+        
+        return quick_select(0, len(nums)-1, len(nums)-k)
