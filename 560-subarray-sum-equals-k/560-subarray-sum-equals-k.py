c444a6a1113ff8ea676154858f463a2cc94eeb3b
Shubham Adep <shubham.adep@yahoo.com>
2022-02-27 12:28:06 -0800

    Time: 256 ms (92.42%), Space: 16.6 MB (85.44%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -1,21 +1,14 @@
-'''
-
-[1,2,3,1,2] k=5
-
-cs += curr_num
-if cs - k in seen:
-    ans += seen[cs-k]
-
-'''
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         seen = collections.defaultdict(int)
-        seen[0] = 1
         cs = 0
+        seen[0] = 1
         ans = 0
+        
         for num in nums:
             cs += num
             if cs - k in seen:
-                ans += seen[cs - k]
+                ans += seen[cs-k]
             seen[cs] += 1
+        
         return ans
