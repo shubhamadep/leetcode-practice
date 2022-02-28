3600e220141744e6d91a501dccf553e2a010d4e1
Shubham Adep <shubham.adep@yahoo.com>
2022-02-27 16:47:24 -0800

    Time: 86 ms (18.34%), Space: 16.3 MB (50.24%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -1,7 +1,7 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         
-        self.dia = 0
+        self.diameter = 0
         def helper(node):
             if not node:
                 return 0
@@ -9,8 +9,9 @@ class Solution:
             left = helper(node.left)
             right = helper(node.right)
             
-            self.dia = max(self.dia, left + right)
+            self.diameter = max(self.diameter, left + right)
+            
             return max(left, right) + 1
         
         helper(root)
-        return self.dia
+        return self.diameter
