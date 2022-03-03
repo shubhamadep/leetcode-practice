4a4df6cd762157fed2e3fd37bde5f7ea990f1a4b
Shubham Adep <shubham.adep@yahoo.com>
2022-03-03 14:18:50 -0800

    Time: 992 ms (87.63%), Space: 39 MB (30.36%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
@@ -1,24 +1,38 @@
-# picture for solution explanation: https://leetcode.com/problems/diagonal-traverse-ii/discuss/597741/Clean-Simple-Easiest-Explanation-with-a-picture-and-code-with-comments
-
-from collections import defaultdict
 class Solution:
     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
-        if not nums:
-            return []
-        
+        store_diagonal_elements = collections.defaultdict(list)
         result = []
         
-        row = len(nums)
-        hashset = set()
-        hashmap = defaultdict(list)
-        
-        for i in range(row):
-            col = len(nums[i])
-            for j in range(col):
-                hashmap[i+j].append(nums[i][j])
-        
-        for k in hashmap.keys():
-            for value in reversed(hashmap[k]):
-                result.append(value)
+        for row in range(len(nums)):
+            for col in range(len(nums[row])):
+                store_diagonal_elements[row+col].append(nums[row][col])
+            
+        for idx in sorted(store_diagonal_elements.keys()):
+            if idx in store_diagonal_elements:
+                result += store_diagonal_elements[idx][::-1]
         
         return result
+    
+    
+# from collections import defaultdict
+# class Solution:
+#     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+#         if not nums:
+#             return []
+        
+#         result = []
+        
+#         row = len(nums)
+#         hashset = set()
+#         hashmap = defaultdict(list)
+        
+#         for i in range(row):
+#             col = len(nums[i])
+#             for j in range(col):
+#                 hashmap[i+j].append(nums[i][j])
+        
+#         for k in hashmap.keys():
+#             for value in reversed(hashmap[k]):
+#                 result.append(value)
+        
+#         return result
