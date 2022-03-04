4d64613ebba84f91ea018010b5fabdebd42b061c
Shubham Adep <shubham.adep@yahoo.com>
2022-03-03 16:28:36 -0800

    Time: 72 ms (43.05%), Space: 14.4 MB (41.97%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
@@ -1,24 +1,23 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         
-        def last(days,index,plan):
-            end = days[index] + plan
-            while index!=len(days) and days[index] < end:
-                index+=1
+        self.minimum_cost = 0
+        def get_next_index(index, next_days):
+            end = days[index] + next_days
+            while index < len(days) and days[index] < end:
+                index += 1
             return index
-
-        def go(days,index,costs,dp):
-            if index == len(days):
+        
+        memo = {}
+        def helper(idx):
+            if idx == len(days):
                 return 0
-            if dp[index] != -1:
-                return dp[index]
-
-            ans = min( costs[0] + go(days,last(days,index,1),costs,dp),
-                       costs[1] + go(days,last(days,index,7),costs,dp),
-                       costs[2] + go(days,last(days,index,30),costs,dp))
-
-            dp[index] = ans
-            return ans
-
-        dp = [-1]*len(days)
-        return go(days,0,costs,dp)
+            
+            if idx in memo:
+                return memo[idx]
+            
+            memo[idx] = min(helper(get_next_index(idx, 1))+costs[0], helper(get_next_index(idx, 7))+costs[1], helper(get_next_index(idx, 30))+costs[2])
+            
+            return memo[idx]
+                                                                       
+        return helper(0)
