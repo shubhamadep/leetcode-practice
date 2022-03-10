fbf325a4fd44a3dd9b2eb4375fc3369ed6fe7659
Shubham Adep <shubham.adep@yahoo.com>
2022-03-09 17:34:47 -0800

    Time: 112 ms (24.31%), Space: 21.6 MB (8.86%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -1,23 +1,4 @@
 class Solution:
-#     def deleteAndEarn(self, nums: List[int]) -> int:
-#         counts = collections.Counter(nums)
-#         total_gain = collections.defaultdict(int)
-#         for item in counts:
-#             total_gain[item] = counts[item]*item
-        
-#         max_item = max(total_gain.keys())
-        
-#         def helper(item):
-#             if item == 0:
-#                 return 0
-#             if item == 1:
-#                 return total_gain[item]
-            
-#             return max(helper(item-2)+total_gain[item], helper(item-1))
-        
-#         return helper(max_item)
-    
-    
     def deleteAndEarn(self, nums: List[int]) -> int:
         counts = collections.Counter(nums)
         total_gain = collections.defaultdict(int)
@@ -26,10 +7,30 @@ class Solution:
         
         max_item = max(total_gain.keys())
         
-        dp = [0]*(max_item+1)
-        dp[1] = total_gain[1]
+        @cache
+        def helper(item):
+            if item == 0:
+                return 0
+            if item == 1:
+                return total_gain[item]
+            
+            return max(helper(item-2)+total_gain[item], helper(item-1))
         
-        for item in range(2, len(dp)):
-            dp[item] = max(dp[item-2]+total_gain[item], dp[item-1])
+        return helper(max_item)
+    
+    
+#     def deleteAndEarn(self, nums: List[int]) -> int:
+#         counts = collections.Counter(nums)
+#         total_gain = collections.defaultdict(int)
+#         for item in counts:
+#             total_gain[item] = counts[item]*item
         
-        return dp[-1]
+#         max_item = max(total_gain.keys())
+        
+#         dp = [0]*(max_item+1)
+#         dp[1] = total_gain[1]
+        
+#         for item in range(2, len(dp)):
+#             dp[item] = max(dp[item-2]+total_gain[item], dp[item-1])
+        
+#         return dp[-1]
