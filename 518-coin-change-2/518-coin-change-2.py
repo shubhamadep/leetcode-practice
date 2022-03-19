351dddf3a16efe6bbb0b1055947f7d0e5c589874
Shubham Adep <shubham.adep@yahoo.com>
2022-03-19 16:26:31 -0700

    Time: 336 ms (49.10%), Space: 23.1 MB (41.44%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -6,15 +6,31 @@ class Solution:
         for i in range(len(coins)+1):
             dp[i][0] = 1
         
-        for row in range(1, len(dp)):
-            for col in range(1, len(dp[0])):
-                coin = coins[row-1]
-                if col >= coin:
-                    dp[row][col] += dp[row][col-coin] + dp[row-1][col]
-                else:
-                    dp[row][col] += dp[row-1][col]
+        prev_dp = [0]*(amount+1)
+        prev_dp[0] = 1
         
-        return dp[-1][-1]
+        for coin in coins:
+            new_dp = [0]*(amount+1)
+            new_dp[0] = 1
+            for amt in range(1, amount+1):
+                if amt >= coin:
+                    new_dp[amt] += new_dp[amt-coin] + prev_dp[amt]
+                else:
+                    new_dp[amt] += prev_dp[amt]
+                    
+            prev_dp = new_dp
+        
+        return new_dp[-1]
+    
+#         for row in range(1, len(dp)):
+#             for col in range(1, len(dp[0])):
+#                 coin = coins[row-1]
+#                 if col >= coin:
+#                     dp[row][col] += dp[row][col-coin] + dp[row-1][col]
+#                 else:
+#                     dp[row][col] += dp[row-1][col]
+        
+#         return dp[-1][-1]
                 
         
 #         @functools.lru_cache(None)
