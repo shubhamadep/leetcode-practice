4bce8e2f1f8de19835aef0d2fce1fd4f204cad76
Shubham Adep <shubham.adep@yahoo.com>
2022-03-22 01:39:40 -0700

    Time: 579 ms (55.07%), Space: 43.8 MB (8.65%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.py
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -1,21 +1,20 @@
 class SnapshotArray:
-
+    
     def __init__(self, length: int):
-        self.snapShot = {}
-        self.snapCount = 0
-
-
+        self.hmap=defaultdict()
+        for i in range(length):
+            self.hmap[i]=[[-1, 0]]
+                #snap id   #value
+        self.snap_id=0
+        
     def set(self, index: int, val: int) -> None:
-        self.snapShot[(self.snapCount, index)] = val
-
-
+        self.hmap[index].append([self.snap_id, val])
+        
     def snap(self) -> int:
-        self.snapCount += 1
-        return self.snapCount -1
-
+        self.snap_id += 1
+        return self.snap_id - 1
 
     def get(self, index: int, snap_id: int) -> int:
-        for snapId in range(snap_id, -1, -1):
-             if (snapId, index) in self.snapShot:
-                return self.snapShot[(snapId, index)]
-        return 0
+    
+        i = bisect.bisect(self.hmap[index], [snap_id + 1]) - 1
+        return self.hmap[index][i][1] 
