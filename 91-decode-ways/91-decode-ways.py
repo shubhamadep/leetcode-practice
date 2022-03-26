246a93ce7b7dd6b95541748ba5c2487b6cc7fb03
Shubham Adep <shubham.adep@yahoo.com>
2022-03-25 21:57:46 -0700

    Time: 62 ms (18.15%), Space: 13.8 MB (83.16%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -22,28 +22,48 @@
 
 #     def numDecodings(self, s: str) -> int:
 #         return self.recursiveWithMemo(0, s)
+    
+    
+# class Solution:
 
+#     @lru_cache(maxsize=None)
+#     def recursiveWithMemo(self, index, s) -> int:
+#         # If you reach the end of the string
+#         # Return 1 for success.
+#         if index == len(s):
+#             return 1
+
+#         # If the string starts with a zero, it can't be decoded
+#         if s[index] == '0':
+#             return 0
+
+#         if index == len(s)-1:
+#             return 1
+        
+#         answer = self.recursiveWithMemo(index + 1, s)
+#         if int(s[index : index + 2]) <= 26:
+#             answer += self.recursiveWithMemo(index + 2, s)
+
+#         return answer
+
+#     def numDecodings(self, s: str) -> int:
+#         return self.recursiveWithMemo(0, s)
+    
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # Array to store the subproblem results
-        dp = [0 for _ in range(len(s) + 1)]
-
-        dp[0] = 1
-        # Ways to decode a string of size 1 is 1. Unless the string is '0'.
-        # '0' doesn't have a single digit decode.
-        dp[1] = 0 if s[0] == '0' else 1
-
-
-        for i in range(2, len(dp)):
-
-            # Check if successful single digit decode is possible.
-            if s[i - 1] != '0':
-                dp[i] = dp[i - 1]
-
-            # Check if successful two digit decode is possible.
-            two_digit = int(s[i - 2 : i])
+        if s[0] == "0":
+            return 0
+    
+        two_back = 1
+        one_back = 1
+        for i in range(1, len(s)):
+            current = 0
+            if s[i] != "0":
+                current = one_back
+            two_digit = int(s[i - 1: i + 1])
             if two_digit >= 10 and two_digit <= 26:
-                dp[i] += dp[i - 2]
-                
-        return dp[len(s)]
-            
+                current += two_back
+            two_back = one_back
+            one_back = current
+        
+        return one_back
