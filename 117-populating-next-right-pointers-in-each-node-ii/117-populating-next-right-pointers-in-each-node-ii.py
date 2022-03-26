44cc8c55e188dcf997ebd9b7a4b74c398b94b4df
Shubham Adep <shubham.adep@yahoo.com>
2022-03-26 13:01:04 -0700

    Time: 48 ms (94.92%), Space: 15.6 MB (5.24%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
@@ -1,41 +1,96 @@
 class Solution:
+    
+    def processChild(self, childNode, prev, leftmost):
+        if childNode:
+            
+            # If the "prev" pointer is alread set i.e. if we
+            # already found atleast one node on the next level,
+            # setup its next pointer
+            if prev:
+                prev.next = childNode
+            else:    
+                # Else it means this child node is the first node
+                # we have encountered on the next level, so, we
+                # set the leftmost pointer
+                leftmost = childNode
+            prev = childNode 
+        return prev, leftmost
+    
     def connect(self, root: Optional['Node']) -> Optional['Node']:
         
         if not root:
             return root
         
-        # Initialize a queue data structure which contains
-        # just the root of the tree
-        Q = collections.deque([root])
+        # The root node is the only node on the first level
+        # and hence its the leftmost node for that level
+        leftmost = root
         
-        # Outer while loop which iterates over 
-        # each level
-        while Q:
+        # We have no idea about the structure of the tree,
+        # so, we keep going until we do find the last level.
+        # The nodes on the last level won't have any children
+        while leftmost:
             
-            # Note the size of the queue
-            size = len(Q)
+            # "prev" tracks the latest node on the "next" level
+            # while "curr" tracks the latest node on the current
+            # level.
+            prev, curr = None, leftmost
             
-            # Iterate over all the nodes on the current level
-            for i in range(size):
+            # We reset this so that we can re-assign it to the leftmost
+            # node of the next level. Also, if there isn't one, this
+            # would help break us out of the outermost loop.
+            leftmost = None
+            
+            # Iterate on the nodes in the current level using
+            # the next pointers already established.
+            while curr:
                 
-                # Pop a node from the front of the queue
-                node = Q.popleft()
+                # Process both the children and update the prev
+                # and leftmost pointers as necessary.
+                prev, leftmost = self.processChild(curr.left, prev, leftmost)
+                prev, leftmost = self.processChild(curr.right, prev, leftmost)
                 
-                # This check is important. We don't want to
-                # establish any wrong connections. The queue will
-                # contain nodes from 2 levels at most at any
-                # point in time. This check ensures we only 
-                # don't establish next pointers beyond the end
-                # of a level
-                if i < size - 1:
-                    node.next = Q[0]
+                # Move onto the next node.
+                curr = curr.next
                 
-                # Add the children, if any, to the back of
-                # the queue
-                if node.left:
-                    Q.append(node.left)
-                if node.right:
-                    Q.append(node.right)
+        return root 
+# class Solution:
+#     def connect(self, root: Optional['Node']) -> Optional['Node']:
         
-        # Since the tree has now been modified, return the root node
-        return root
+#         if not root:
+#             return root
+        
+#         # Initialize a queue data structure which contains
+#         # just the root of the tree
+#         Q = collections.deque([root])
+        
+#         # Outer while loop which iterates over 
+#         # each level
+#         while Q:
+            
+#             # Note the size of the queue
+#             size = len(Q)
+            
+#             # Iterate over all the nodes on the current level
+#             for i in range(size):
+                
+#                 # Pop a node from the front of the queue
+#                 node = Q.popleft()
+                
+#                 # This check is important. We don't want to
+#                 # establish any wrong connections. The queue will
+#                 # contain nodes from 2 levels at most at any
+#                 # point in time. This check ensures we only 
+#                 # don't establish next pointers beyond the end
+#                 # of a level
+#                 if i < size - 1:
+#                     node.next = Q[0]
+                
+#                 # Add the children, if any, to the back of
+#                 # the queue
+#                 if node.left:
+#                     Q.append(node.left)
+#                 if node.right:
+#                     Q.append(node.right)
+        
+#         # Since the tree has now been modified, return the root node
+#         return root
