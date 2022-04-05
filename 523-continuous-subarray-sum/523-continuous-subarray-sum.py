e3a7cc99342e4a12405f69a8da42924f3119dc6d
Shubham Adep <shubham.adep@yahoo.com>
2022-04-04 20:01:38 -0700

    Time: 1012 ms (75.73%), Space: 33.6 MB (19.56%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.py
@@ -1,16 +1,22 @@
+'''
+[24,2,4,6,7], k = 6
+[23,25,29,35,42]
+[5,1,5,5,0]
+
+'''
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        prefix_sums = defaultdict(lambda:float(inf)) 
-
-        running_sum = 0
-        for i, n in enumerate(nums):
-            running_sum += n
-            mod_sum = running_sum%k
-
-            if i >= 1 and (mod_sum == 0 or prefix_sums[mod_sum] < i - 1):
+        seen = collections.defaultdict(lambda: float('inf'))
+        
+        cs = 0
+        for i in range(len(nums)):
+            num = nums[i]
+            cs += num
+            target = cs % k
+            
+            if i > 0 and target == 0 or seen[target] < i - 1:
                 return True
             
-            #We wouldn't need to keep track of minimum position if we could have subarray of size 1
-            prefix_sums[mod_sum] = min(prefix_sums[mod_sum], i)
-                
+            seen[target] = min(i, seen[target])
+        
         return False
